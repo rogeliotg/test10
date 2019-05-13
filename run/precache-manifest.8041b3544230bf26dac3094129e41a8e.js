aa46202b0cd02c8989fb1d642a9d6c23d6a6e830
rogeliotg <rogeliotg@fitsaas.com>
2019-05-13 10:29:13 +0200

    run on github

---
+++ run/precache-manifest.8041b3544230bf26dac3094129e41a8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4519a8ebc0887b8c46e12b2e373347bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a605bff60f431a8460f",
+    "url": "/static/css/main.8493514f.chunk.css"
+  },
+  {
+    "revision": "fb1cc8c897d975d6dc26",
+    "url": "/static/js/2.80dfb272.chunk.js"
+  },
+  {
+    "revision": "0a605bff60f431a8460f",
+    "url": "/static/js/main.3c379c46.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
